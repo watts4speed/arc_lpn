30bedcc7f276be749a9743b5a5ac9dabee7adf84
Clement Bonnet <clement.bonnet16@gmail.com>
2024-11-13 22:37:13 +0100

    fix: setup py

--- setup.py
+++ setup.py
@@ -6,6 +6,16 @@ with open("requirements.txt") as f:
 setup(
     name="lpn",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["src", "src.*"]),
+    package_dir={"": "src"},
     install_requires=requirements,
+    python_requires=">=3.6",
+    url="https://github.com/clement-bonnet/lpn",
+    author="Clement Bonnet",
+    author_email="clement.bonnet16@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+    ],
 )
